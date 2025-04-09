5ab71f2f6fcf8f2a07f160bc4cd9cc62066752df
Thanh Tuyen <mikun1402@gmail.com>
2025-04-09 22:24:13 +0700

    fix css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-    component: LayoutComponent,
   },
   {
     path: 'home',
